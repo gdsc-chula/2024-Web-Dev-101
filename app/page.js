d8009686bb22b43fa1f8f1fefcb6360c93da78ce
Kafiling <panthawit.k@gmail.com>
2024-10-21 01:10:19 +0700

    1st draft of homepage

--- app/page.js
+++ app/page.js
@@ -2,9 +2,40 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-white">
-      <h2 className="text-black">Test</h2>
-      <h2 className="text-black">{process.env.apiKey}</h2>
-    </div>
+    <>
+      <section className="bg-gray-900 text-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-3xl text-center">
+            <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
+              Brew Conversations,
+              {/*Use block to forcefully break the line no matter the screen size*/}
+              <span className="sm:block"> Share Opinions </span>
+            </h1>
+
+            <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+              Roast & Respond is a polling app that blends virtual coffee chats
+              with engaging discussions. Brew conversations, share opinions, and
+              gather instant feedback in a cozy, cafe-inspired space.
+            </p>
+
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
+              <a
+                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                href="#"
+              >
+                Login with Google
+              </a>
+
+              <a
+                className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
+                href="#"
+              >
+                Take a Seat 🪑☕
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
