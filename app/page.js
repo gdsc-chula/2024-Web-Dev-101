9123a6291a94a06bae6a794e98e9361e10e92ff6
Kafiling <panthawit.k@gmail.com>
2024-10-22 19:10:29 +0700

    Implement Header

--- app/page.js
+++ app/page.js
@@ -1,15 +1,17 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <section className="bg-gray-900 text-white">
         <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
           <div className="mx-auto max-w-3xl text-center">
             <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">
               Brew Conversations,
               {/*Use block to forcefully break the line no matter the screen size*/}
-              <span className="sm:block"> Share Opinions </span>
+              <span className="sm:block"> Share Stories </span>
             </h1>
 
             <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
@@ -30,7 +32,7 @@ export default function Home() {
                 className="block w-full rounded border border-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto"
                 href="#"
               >
-                Take a Seat 🪑☕
+                Take a Seat First 🪑☕
               </a>
             </div>
           </div>
